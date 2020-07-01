7c31c60d33cadffb23efb3e6ea63909cd0306327
Ravi <47631874+rnarukulla2@users.noreply.github.com>
2020-07-01 17:44:13 +0530

    build fix- updated test run command

--- test_cases/test_fe.py
+++ test_cases/test_fe.py
@@ -2,7 +2,7 @@ import unittest
 import pandas as pd
 import numpy as np
 from AV_clubmahindra_feature_eng.fe_modules import date_col_derivation, date_diff, agg_col, groupby_time_delta
-from pandas.util.testing import assert_frame_equal, assert_series_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 class test_fe_modules(unittest.TestCase):
 
